d7958a2ff959e7388fe60831d3f1f1eb713c0239
JoseSantosJ <157178612+JoseSantosJ@users.noreply.github.com>
2024-03-24 17:39:06 -0300

    qwre1

--- financeiro/src/main/java/br/com/projeto1/financeiro/controle/EntradasControle.java
+++ financeiro/src/main/java/br/com/projeto1/financeiro/controle/EntradasControle.java
@@ -44,9 +44,9 @@ public class EntradasControle {
     }
 
 
-    @GetMapping("/somadasentradas")
+   /*  @GetMapping("/somadasentradas")
     public Double somar(){
         return es.somadasentradas();
-    }
+    }*/
     
 }
