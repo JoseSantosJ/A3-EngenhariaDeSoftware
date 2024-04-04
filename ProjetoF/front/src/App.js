b5544b437d4e2c256b3d8a59fe123a9038011323
JoseSantosJ <157178612+JoseSantosJ@users.noreply.github.com>
2024-04-04 10:58:08 -0300

    version 0.1.5

--- ProjetoF/front/src/App.js
+++ ProjetoF/front/src/App.js
@@ -3,6 +3,7 @@ import{BrowserRouter, Routes, Link, Route} from 'react-router-dom'
 import Gastos from './components/gastos/Gastos';
 import Entradas from './components/entradas/Entradas';
 import GastosMes from './components/gastos/GastosMes';
+import EntradasMes from './components/entradas/EntradasMes';
  
 
 
@@ -18,12 +19,15 @@ function App() {
         <li><Link to="/telaDeCadastro">Gastos</Link>
         <li><Link to="/gastosMensais">Gastos por mes</Link></li>
         </li>
-        <li><Link to="/telaDeEntradas">Entradas</Link></li>
+        <li><Link to="/telaDeEntradas">Entradas</Link>
+        <li><Link to="/entradasMensais">Entradas por mes</Link></li>
+        </li>
     </ul>
     <Routes>
       <Route path='/telaDeCadastro' element={<Gastos/>}></Route>
       <Route path='/telaDeEntradas' element={<Entradas/>}></Route>
       <Route path='/gastosMensais' element={<GastosMes/>}></Route>
+      <Route path='/entradasMensais' element={<EntradasMes/>}></Route>
     </Routes>
     </BrowserRouter>
     </div>
